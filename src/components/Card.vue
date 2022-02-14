9df05d57a9b28346223b16b3d6546086aae14710
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-14 17:44:31 +0100

    implemented flags

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,7 @@
       <li v-for="(movie,index) in moviesSelected" :key="movie.id || index">
        <h2>{{movie.title}}</h2>
        <h3>{{movie.original_title}}</h3>
-       <div><p>Language:</p>{{movie.original_language}}<img :src="`../assets/img/${movie.original_language}.png`" alt=""></div>
+       <div><p>Language:</p>{{movie.original_language}}<img v-if="(movie.original_language=='it' || movie.original_language=='en')" :src="require(`../assets/img/${movie.original_language}.png`)" alt=""></div>
        <h6>Rating: {{movie.vote_average}}</h6>
       </li>
   </div>
@@ -13,7 +13,8 @@
 export default {
 name:"Card",
 props:["moviesSelected"],
-
+methods:{
+},
 }
 </script>
 
