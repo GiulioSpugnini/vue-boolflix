f872cd719b5e1f4420aa15667cdb69aa6809fc92
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-14 13:38:55 +0100

    linked input text to query

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <template>
   <ul>
-    <Search/>
+    <Search :movies="movies" :query="query"  @input-Search-Text="setSelectedQuery"/>
   </ul>
 </template>
 
 <script>
 import Search from './components/Search.vue';
+import axios from 'axios';
 export default {
   components: { Search },
   name: 'App',
+  data() {
+    return{
+      movies:[],
+      api_key:"dfe0b56363d3d8cf312014e87a1dc974",
+      query: "",
+    }
+  },
+  methods:{
+    searchMovie(){
+      const config={
+        params:{
+          api_key: this.api_key,
+          query: this.query,
+          language: 'it-IT',
+        },
+      }
+
+      axios.get(`https://api.themoviedb.org/3/search/movie`, config).then((res)=>{
+        this.movies=res.data.results;
+      });
+    },
+    setSelectedQuery(query){
+      this.query=query;
+    },
+  }
   
 }
 </script>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,17 +1,26 @@
 <template>
   <li>
-    <input type="text" placeholder="Cerca..">
+    <input
+      @keyup="inputSearch"
+      v-model="query"
+      type="text"
+      placeholder="Cerca.."
+    />
     <button>Cerca</button>
   </li>
 </template>
 
 <script>
 export default {
-  name: 'Search',
-  
-}
+  name: "Search",
+  props: ["movies", "query"],
+  methods: {
+    inputSearch() {
+      this.$emit("input-Search-Text", this.query);
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-
 </style>
