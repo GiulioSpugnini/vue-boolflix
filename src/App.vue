6a3c7495105f38353b23c8a4d99a7db12a7524cc
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-15 15:19:30 +0100

    added poster_path and imported bootstrap

--- src/App.vue
+++ src/App.vue
@@ -47,4 +47,5 @@ export default {
 </script>
 
 <style lang="scss">
+@import "./assets/scss/style.scss";
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,6 +2,9 @@
   <div>
     <li v-for="(movie, index) in moviesSelected" :key="movie.id || index">
       <h2>{{ movie.title || movie.name }}</h2>
+      <figure>
+        <img :src="`http://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt="">
+      </figure>
       <h3>{{ movie.original_title || movie.original_name }}</h3>
       <div>
         <p>Language:</p>
@@ -14,7 +17,7 @@
           alt=""
         />
       </div>
-      <h6>Rating: {{ movie.vote_average }}</h6>
+      <h6>Rating: {{ Math.ceil(movie.vote_average) }}</h6>
     </li>
   </div>
 </template>
