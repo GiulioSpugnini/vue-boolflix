36117a508ed31748c6112dff33602cf0b8fc19e4
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-16 12:39:55 +0100

    added deafult background img if doesn't exist in api

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -10,7 +10,7 @@
           <p v-else>{{ item.original_language }}</p>
         </div>
         <h6>
-          Rating: {{setStar}}
+          Rating:
           <i v-for="(vote,index) in 5" :key="index" class="fa-star" :class="index<setStar? 'fa-solid':'fa-regular'"></i>
         </h6>
       </li>
@@ -35,7 +35,8 @@ export default {
       return this.flags.includes(this.item.original_language);
     },
     postSrc() {
-      return `http://image.tmdb.org/t/p/w342/${this.item.poster_path}`;
+      if(!this.item.poster_path)return `https://www.altavod.com/assets/images/poster-placeholder.png`;
+       return `http://image.tmdb.org/t/p/w342/${this.item.poster_path}`;
     },
     setStar() {
       return Math.ceil(this.item.vote_average/2);
